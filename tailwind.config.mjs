26b0c308a742e53e711eaed9c0b7aa6ee020b625
Mateus Wolkmer <mateuswsouza@gmail.com>
2024-03-10 22:06:41 -0300

    add fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import plugin from "tailwindcss/plugin";
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
 	theme: {
-		extend: {},
+		fontFamily: {
+			body: ["Rubik", ...defaultTheme.fontFamily.sans],
+			display: ["Staatliches", ...defaultTheme.fontFamily.sans],
+		},
 	},
-	plugins: [],
+	plugins: [
+		plugin(({ addBase, theme }) => {
+			addBase({
+				h1: { fontFamily: theme("fontFamily.display") },
+				h2: { fontFamily: theme("fontFamily.display") },
+				h3: { fontFamily: theme("fontFamily.display") },
+				h4: { fontFamily: theme("fontFamily.display") },
+				h5: { fontFamily: theme("fontFamily.display") },
+				h6: { fontFamily: theme("fontFamily.display") },
+			});
+		}),
+	],
 };
