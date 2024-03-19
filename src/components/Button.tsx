4ec06679c5ba7e7b23ad7753e697524c365b9e6b
Mateus Wolkmer <mateuswsouza@gmail.com>
2024-03-18 23:43:06 -0300

    adjust colors and create Link

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,66 +3,66 @@ import type { Component, JSX } from "solid-js";
 import { twMerge } from "tailwind-merge";
 
 const buttonVariants = cva(
-	"transition-all hover:scale-105 hover:-rotate-1 border-4 border-b-8 border-black rounded-xl flex gap-2 group",
-	{
-		variants: {
-			variant: {
-				cta: "text-6xl bg-pink-400 hover:bg-pink-300 py-5 px-20",
-				nav: "text-3xl bg-blue-300 hover:bg-blue-200 py-2 px-5",
-			},
-		},
-	},
+  "transition-all hover:scale-105 hover:-rotate-1 border-4 border-b-8 border-black rounded-xl flex gap-2 group",
+  {
+    variants: {
+      variant: {
+        cta: "text-6xl bg-pink hover:bg-pink-300 py-5 px-20",
+        nav: "text-3xl bg-blue hover:bg-blue-300 py-2 px-5",
+      },
+    },
+  }
 );
 
 export type ButtonProps = JSX.ButtonHTMLAttributes<HTMLButtonElement> &
-	VariantProps<typeof buttonVariants>;
+  VariantProps<typeof buttonVariants>;
 
 export const Button: Component<ButtonProps> = ({
-	variant,
-	class: classes,
-	children,
-	...rest
+  variant,
+  class: classes,
+  children,
+  ...rest
 }) => {
-	return (
-		<button
-			type="button"
-			class={twMerge(buttonVariants({ variant }), classes)}
-			{...rest}
-		>
-			{children}
-			{/* TODO replace > with icon */}
-			{variant === "nav" && (
-				<span class="text-3xl transition-transform group-hover:scale-125">
-					{">"}
-				</span>
-			)}
-		</button>
-	);
+  return (
+    <button
+      type="button"
+      class={twMerge(buttonVariants({ variant }), classes)}
+      {...rest}
+    >
+      {children}
+      {/* TODO replace > with icon */}
+      {variant === "nav" && (
+        <span class="text-3xl transition-transform group-hover:scale-125">
+          {">"}
+        </span>
+      )}
+    </button>
+  );
 };
 
 export type ButtonLink = JSX.AnchorHTMLAttributes<HTMLAnchorElement> &
-	VariantProps<typeof buttonVariants>;
+  VariantProps<typeof buttonVariants>;
 
 export const ButtonLink: Component<ButtonLink> = ({
-	href,
-	variant,
-	class: classes,
-	children,
-	...rest
+  href,
+  variant,
+  class: classes,
+  children,
+  ...rest
 }) => {
-	return (
-		<a
-			href={href}
-			class={twMerge(buttonVariants({ variant }), classes)}
-			{...rest}
-		>
-			{children}
-			{/* TODO replace > with icon */}
-			{variant === "nav" && (
-				<span class="text-3xl transition-transform group-hover:scale-125">
-					{">"}
-				</span>
-			)}
-		</a>
-	);
+  return (
+    <a
+      href={href}
+      class={twMerge(buttonVariants({ variant }), classes)}
+      {...rest}
+    >
+      {children}
+      {/* TODO replace > with icon */}
+      {variant === "nav" && (
+        <span class="text-3xl transition-transform group-hover:scale-125">
+          {">"}
+        </span>
+      )}
+    </a>
+  );
 };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,33 +3,68 @@ import plugin from "tailwindcss/plugin";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
-	theme: {
-		fontFamily: {
-			body: ["Outfit Variable", ...defaultTheme.fontFamily.sans],
-			display: ["Staatliches", ...defaultTheme.fontFamily.sans],
-		},
-		extend: {
-			spacing: {
-				header: "100px",
-				footer: "400px",
-			},
-		},
-	},
-	plugins: [
-		plugin(({ addBase, theme }) => {
-			addBase({
-				h1: { fontFamily: theme("fontFamily.display") },
-				h2: { fontFamily: theme("fontFamily.display") },
-				h3: { fontFamily: theme("fontFamily.display") },
-				h4: { fontFamily: theme("fontFamily.display") },
-				h5: { fontFamily: theme("fontFamily.display") },
-				h6: { fontFamily: theme("fontFamily.display") },
-				button: { fontFamily: theme("fontFamily.display") },
-				a: { fontFamily: theme("fontFamily.display") },
-				p: { fontSize: theme("fontSize.2xl") },
-				span: { fontSize: theme("fontSize.2xl") },
-			});
-		}),
-	],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      body: ["Outfit Variable", ...defaultTheme.fontFamily.sans],
+      display: ["Staatliches", ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      spacing: {
+        header: "100px",
+        footer: "400px",
+      },
+      colors: {
+        pink: {
+          100: "#f6ecf4",
+          200: "#eed9e9",
+          300: "#e5c5df",
+          400: "#ddb2d4",
+          DEFAULT: "#d49fc9",
+          600: "#aa7fa1",
+          700: "#7f5f79",
+          800: "#554050",
+          900: "#2a2028",
+        },
+        blue: {
+          100: "#eff8f8",
+          200: "#dff2f2",
+          300: "#ceebeb",
+          400: "#bee5e5",
+          DEFAULT: "#aedede",
+          600: "#8bb2b2",
+          700: "#688585",
+          800: "#465959",
+          900: "#232c2c",
+        },
+        yellow: {
+          100: "#fef9e4",
+          200: "#fcf3c8",
+          300: "#fbecad",
+          400: "#f9e691",
+          DEFAULT: "#f8e076",
+          600: "#c6b35e",
+          700: "#958647",
+          800: "#635a2f",
+          900: "#322d18",
+        },
+      },
+    },
+  },
+  plugins: [
+    plugin(({ addBase, theme }) => {
+      addBase({
+        h1: { fontFamily: theme("fontFamily.display") },
+        h2: { fontFamily: theme("fontFamily.display") },
+        h3: { fontFamily: theme("fontFamily.display") },
+        h4: { fontFamily: theme("fontFamily.display") },
+        h5: { fontFamily: theme("fontFamily.display") },
+        h6: { fontFamily: theme("fontFamily.display") },
+        button: { fontFamily: theme("fontFamily.display") },
+        a: { fontFamily: theme("fontFamily.display") },
+        p: { fontSize: theme("fontSize.2xl") },
+        span: { fontSize: theme("fontSize.2xl") },
+      });
+    }),
+  ],
 };
