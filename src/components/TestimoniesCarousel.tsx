52090fe8aca556ed8342a154a5817132686e130c
Mateus Wolkmer <mateuswsouza@gmail.com>
2024-03-20 14:35:09 -0300

    animate TestimoniesCarousel

--- src/components/TestimoniesCarousel.tsx
+++ src/components/TestimoniesCarousel.tsx
@@ -1,4 +1,12 @@
-import { type Component, For, Show, createMemo, createSignal } from "solid-js";
+import { createPresence } from "@solid-primitives/presence";
+import {
+  type Component,
+  For,
+  Show,
+  createMemo,
+  createSignal,
+  createEffect,
+} from "solid-js";
 import { twMerge } from "tailwind-merge";
 
 export type Testimony = {
@@ -22,28 +30,78 @@ export const TestimoniesCarousel: Component<TestimoniesCarousel> = ({
   const [selected, setSelected] = createSignal(initialSelected);
   const selectedTestimony = createMemo(() => testimonies[selected()]);
 
+  const [commentRefs, setCommentRefs] = createSignal<HTMLParagraphElement[]>(
+    []
+  );
+  const [commentsWrapperRef, setCommentsWrapperRef] =
+    createSignal<HTMLDivElement>();
+
+  createEffect(() => {
+    const wrapper = commentsWrapperRef();
+    if (selected() && wrapper) {
+      const selectedCommentHeight = commentRefs()[selected()].scrollHeight;
+      wrapper.style.height = `${selectedCommentHeight}px`;
+    }
+  });
+
   return (
-    // TODO add height change transition
     <div class="flex flex-col items-center w-full">
-      {/* TODO add big " in bg */}
-      <div class="w-full bg-blue-200 rounded-xl border-4 border-black flex items-center justify-center px-10 py-20">
-        {/* TODO add text transition */}
-        <p>{selectedTestimony().comment}</p>
+      <div class="w-full bg-blue-200 rounded-xl border-4 border-black flex items-center justify-center px-10 py-20 relative">
+        <span class="absolute -top-20 -left-20 text-blue text-[400px] font-display leading-none">
+          “
+        </span>
+        <span class="absolute top-28 -right-20 text-blue text-[400px] font-display leading-none">
+          ”
+        </span>
+        <div ref={setCommentsWrapperRef} class="relative w-full transition-all">
+          <For each={testimonies}>
+            {(testimony, i) => {
+              const state = createMemo(() =>
+                selected() === i()
+                  ? "selected"
+                  : selected() > i()
+                  ? "left"
+                  : "right"
+              );
+
+              return (
+                <p
+                  ref={(ref) => {
+                    const newCommentRefs = commentRefs();
+                    newCommentRefs[i()] = ref;
+                    setCommentRefs(newCommentRefs);
+                  }}
+                  class={twMerge(
+                    "m-auto transition-all duration-300 absolute left-0 right-0 top-0 text-center",
+                    state() === "selected" && "opacity-1 translate-x-0",
+                    state() === "left" && "opacity-0 -translate-x-10",
+                    state() === "right" && "opacity-0 translate-x-10"
+                  )}
+                >
+                  {testimony.comment}
+                </p>
+              );
+            }}
+          </For>
+        </div>
       </div>
       <div class="flex flex-col gap-1 items-center -mt-12">
-        <div class="flex gap-4 items-center h-24 mb-20">
+        <div class="flex gap-6 items-center h-24 mb-20">
           <For each={testimonies}>
             {(testimony, i) => {
               const isSelected = createMemo(() => selected() === i());
+
+              const { isVisible, isMounted } = createPresence(isSelected, {
+                transitionDuration: 500,
+              });
+
               return (
-                <div
-                  class={twMerge("relative", isSelected() ? "w-24" : "w-16")}
-                >
+                <div class="relative">
                   <button
                     type="button"
                     class={twMerge(
-                      "bg-gray-200 rounded-full border-4 border-black transition-all size-16",
-                      selected() === i() && "size-24"
+                      "bg-gray-200 rounded-t-[100%] rounded-b-3xl border-4 border-b-8 border-black transition-all transform size-16",
+                      isSelected() ? "size-24" : "hover:scale-110"
                     )}
                     disabled={isSelected()}
                     title={`${isSelected() ? "" : "Selecionar "}${
@@ -51,17 +109,23 @@ export const TestimoniesCarousel: Component<TestimoniesCarousel> = ({
                     }`}
                     onClick={() => setSelected(i)}
                   />
-                  {/* TODO add transition when shown */}
-                  <Show when={isSelected()}>
-                    <div class="flex flex-col items-center text-center absolute top-28 left-0 right-0">
-                      <span class="text-nowrap">
-                        {selectedTestimony().name}
+                  <Show when={isMounted()}>
+                    <div
+                      class={twMerge(
+                        "flex flex-col items-center text-center absolute top-28 left-0 right-0 transform origin-top duration-500 transition-all",
+                        isVisible()
+                          ? "opacity-1 scale-100"
+                          : "opacity-0 scale-0"
+                      )}
+                    >
+                      <span class="text-nowrap font-medium">
+                        {testimony.name}
                       </span>
                       <span class="text-lg text-nowrap">
-                        adotou {selectedTestimony().adopted}
-                        <Show when={selectedTestimony().adoptedPlus}>
+                        adotou {testimony.adopted}
+                        <Show when={testimony.adoptedPlus}>
                           {" "}
-                          + {selectedTestimony().adoptedPlus}
+                          + {testimony.adoptedPlus}
                         </Show>
                       </span>
                     </div>
