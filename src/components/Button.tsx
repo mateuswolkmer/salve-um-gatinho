059161dba06b4b1c42baa3cf781add70c42a53bc
Mateus Wolkmer <mateuswsouza@gmail.com>
2024-03-23 12:48:10 -0300

    add Contact form

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,12 +3,13 @@ import type { Component, JSX } from "solid-js";
 import { twMerge } from "tailwind-merge";
 
 const buttonVariants = cva(
-  "transition-all hover:scale-105 hover:shadow-lg active:scale-95 active:shadow-none hover:-rotate-1 border-4 border-b-8 active:border-b-4 active:mb-[4px] border-black rounded-xl flex gap-2 group",
+  "transition-all hover:scale-105 hover:shadow-lg active:scale-95 active:shadow-none hover:-rotate-1 border-4 border-b-8 active:border-b-4 active:mb-[4px] border-black rounded-xl flex gap-2 group focus:outline-4 focus:outline-offset-2 focus:outline-dashed focus:outline-pink active:outline-none",
   {
     variants: {
       variant: {
         cta: "text-6xl bg-pink hover:bg-pink-300 active:bg-pink-600 py-5 px-20",
         nav: "text-3xl bg-blue hover:bg-blue-300 active:bg-blue-600 py-2 px-5",
+        form: "text-4xl bg-yellow hover:bg-yellow-300 active:bg-yellow-600 py-2 px-6",
       },
     },
   }
@@ -30,7 +31,6 @@ export const Button: Component<ButtonProps> = ({
       {...rest}
     >
       {children}
-      {/* TODO replace > with icon */}
       {variant === "nav" && (
         <span class="text-3xl transition-transform group-hover:scale-125">
           {">"}
