b91ef42f4204994dd897f1f48bba442177ed1219
Mateus Wolkmer <mateuswsouza@gmail.com>
2024-03-24 13:34:22 -0300

    add Highlight

---
+++ src/components/Highlight.tsx
@@ -0,0 +1,20 @@
+import type { Component, JSX } from "solid-js";
+import { twMerge } from "tailwind-merge";
+
+export type HighlightProps = JSX.HTMLAttributes<HTMLSpanElement>;
+
+export const Highlight: Component<HighlightProps> = ({
+  class: classes,
+  children,
+}) => {
+  return (
+    <em
+      class={twMerge(
+        "relative not-italic after:absolute after:inset-0 after:top-1/3 after:h-1/2 after:bg-yellow-400 after:-z-10 after:-rotate-2 after:scale-[101%]",
+        classes
+      )}
+    >
+      {children}
+    </em>
+  );
+};
