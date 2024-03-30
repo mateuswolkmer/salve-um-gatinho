70efe251d41a3a838d2cbecf30023965a9a5bd95
Mateus Wolkmer <mateuswsouza@gmail.com>
2024-03-30 16:33:52 -0300

    adopt form animation

---
+++ src/components/pages/Cat/AdoptForm.tsx
@@ -0,0 +1,99 @@
+import { createSignal, type Component, createEffect } from "solid-js";
+import { Button } from "../../Button";
+
+export type AdoptFormProps = {};
+
+export const AdoptForm: Component<AdoptFormProps> = () => {
+  const [formOpened, setFormOpened] = createSignal(false);
+
+  // FIXME setting opacity class based on signal change didn't work
+  let buttonRef: HTMLButtonElement | undefined;
+  createEffect(() => {
+    if (formOpened() && buttonRef) {
+      buttonRef.style.transition = "none";
+      buttonRef.style.opacity = "0";
+      buttonRef.style.pointerEvents = "none";
+      buttonRef.style.zIndex = "-1";
+    }
+  });
+
+  return (
+    <div class="flex flex-col items-start gap-4 relative">
+      <Button
+        ref={buttonRef}
+        class="absolute left-0 top-0 transition-all z-10"
+        variant="cta"
+        onClick={() => setFormOpened(true)}
+      >
+        Me adote
+      </Button>
+      <div
+        classList={{
+          "border-4 border-black rounded-xl bg-pink p-4 md:p-6 overflow-hidden w-full transition-all duration-300":
+            true,
+          "max-h-14 max-w-64 sm:max-w-[20rem] sm:max-h-20 m-4": !formOpened(),
+          "max-w-full max-h-[35rem]": formOpened(),
+        }}
+      >
+        <h2
+          classList={{
+            "text-nowrap transition-all duration-300 mb-4 -z-10": true,
+            "text-5xl sm:text-6xl px-14 -mt-4 -ml-2 -rotate-1": !formOpened(),
+            "text-4xl": formOpened(),
+          }}
+        >
+          Me adote
+        </h2>
+        {formOpened() && (
+          <form
+            method="post"
+            autocomplete="on"
+            class="flex flex-col gap-2 w-full"
+          >
+            <div class="flex flex-col gap-1">
+              <label for="name" class="text-lg font-semibold">
+                Nome
+              </label>
+              <input
+                id="name"
+                name="name"
+                type="text"
+                placeholder="Maria"
+                required
+                class="transition-all border-4 border-b-8 focus:border-b-4 focus:mb-1 transform focus:scale-[102%] border-black hover:scale-[99%] hover:shadow-lg focus:shadow-none rounded-xl p-2 text-xl"
+              />
+            </div>
+            <div class="flex flex-col gap-1">
+              <label for="email" class="text-lg font-semibold">
+                Email
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="maria@gmail.com"
+                required
+                class="transition-all border-4 border-b-8 focus:border-b-4 focus:mb-1 transform focus:scale-[102%] border-black hover:scale-[99%] hover:shadow-lg focus:shadow-none rounded-xl p-2 text-xl"
+              />
+            </div>
+            <div class="flex flex-col gap-1">
+              <label for="subject" class="text-lg font-semibold">
+                Já adotou pelo Salve?
+              </label>
+              <input
+                id="subject"
+                name="subject"
+                type="text"
+                placeholder="Sim/Não"
+                class="transition-all border-4 border-b-8 focus:border-b-4 focus:mb-1 transform focus:scale-[102%] border-black hover:scale-[99%] hover:shadow-lg focus:shadow-none rounded-xl p-2 text-xl"
+              />
+            </div>
+            <Button variant="form" class="self-start mt-4" type="submit">
+              Enviar
+            </Button>
+          </form>
+        )}
+      </div>
+    </div>
+  );
+};
