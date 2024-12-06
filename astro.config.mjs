7b237974464f0f270989223d8cdd3ba9ee3c2136
Mateus Wolkmer <mateuswsouza@gmail.com>
2024-12-06 16:57:14 -0300

    Revert "up packages"
    This reverts commit 4ebb359d4c3aeda73cf3aee1f00255a55e0a467d.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import reactJs from "@astrojs/react";
-import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
   integrations: [tailwind(), solidJs(), reactJs()],
 });
