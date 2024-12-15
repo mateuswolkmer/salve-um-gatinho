eab432b6c8ef62e4a365dca5247ccdda6955d206
Mateus Wolkmer <mateuswsouza@gmail.com>
2024-12-15 01:03:50 -0300

    reconfigure og generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,30 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import reactJs from "@astrojs/react";
+import opengraphImages, { presets } from "astro-opengraph-images";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs(), reactJs()],
+  integrations: [
+    tailwind(),
+    solidJs(),
+    reactJs(),
+    opengraphImages({
+      // TODO change to prod when launched
+      site: "https://salve-um-gatinho.vercel.app",
+      options: {
+        fonts: [
+          {
+            name: "Outfit",
+            weight: 400,
+            style: "normal",
+            data: fs.readFileSync(
+              "node_modules/@fontsource-variable/outfit/files/outfit-latin-wght-normal.woff2"
+            ),
+          },
+        ],
+      },
+      render: presets.blackAndWhite,
+    }),
+  ],
 });
