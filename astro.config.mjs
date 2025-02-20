a357abbdd61717615e2227367b3a55d47d1ceda4
Mateus Wolkmer <mateuswsouza@gmail.com>
2025-02-20 00:31:07 -0300

    add astro umami integration (not official)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import reactJs from "@astrojs/react";
+import umami from "@yeskunall/astro-umami";
+
 // import vercel from "@astrojs/vercel";
 
 // import opengraphImages from "astro-opengraph-images";
@@ -17,6 +19,7 @@ export default defineConfig({
     tailwind(),
     solidJs(),
     reactJs(),
+    umami({ id: "b0ecec58-ff13-4ab2-b41d-1ceac7e8b7b4" }),
     // TODO come back to improved RTE images later
     // opengraphImages({
     //   options: {
