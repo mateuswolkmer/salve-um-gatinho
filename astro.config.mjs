4ba80c210f485b9058a0d8dcf51729c7f34860e2
Mateus Wolkmer <mateuswsouza@gmail.com>
2025-02-23 18:33:56 -0300

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import reactJs from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-
-// import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel";
 
 // import opengraphImages from "astro-opengraph-images";
 // import { catsOg } from "./src/og";
@@ -13,7 +12,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://salveumgatinho.org/",
   // output: "server",
-  // adapter: vercel(),
+  adapter: vercel(),
   integrations: [tailwind(), solidJs(), reactJs(), sitemap()],
   // TODO bring back to improved OG images later
   // opengraphImages({
